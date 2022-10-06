e04bb3c5e43d499edec4d94b4f15895d273cae25
SathyakumarDev <zenmedi0023@gmail.com>
2022-10-07 04:29:33 +0530

    modified version

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Deployment is happening here.</p>
+        <p>BREAKING CHANGES!</p>
       </header>
     </div>
   );
